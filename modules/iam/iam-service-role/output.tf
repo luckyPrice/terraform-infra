3c2ee00092075e688d88b90879b5f39e2e7a8707
luckyPrice <luckyprice1103@naver.com>
2025-03-25 17:26:46 +0900

    naming

--- modules/iam/iam-service-role/output.tf
+++ modules/iam/iam-service-role/output.tf
@@ -1,6 +1,6 @@
-output "ec2-iam-role-profile" {
+output "ec2-iam-role-profile-name" {
   value = aws_iam_instance_profile.ec2-iam-role-profile.name
 }
-output "ec2-iam-role-name" {
+output "ec2_iam_role_name" {
   value = aws_iam_role.ec2-iam-role.name
 }
