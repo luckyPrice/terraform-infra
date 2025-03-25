786e90c9a453cfa3b67d4aaf66041a3622936197
luckyPrice <luckyprice1103@naver.com>
2025-03-25 16:44:00 +0900

    ec2

--- modules/iam/iam-service-role/output.tf
+++ modules/iam/iam-service-role/output.tf
@@ -1,5 +1,5 @@
 output "ec2-iam-role-profile" {
-  value = aws_iam_instance_profile.ec2-iam-role-profile
+  value = aws_iam_instance_profile.ec2-iam-role-profile.name
 }
 output "ec2-iam-role-name" {
   value = aws_iam_role.ec2-iam-role.name
