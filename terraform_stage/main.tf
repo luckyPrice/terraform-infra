48a60f9d28bb983b4446d26b3d91498be3f9d6fd
luckyPrice <luckyprice1103@naver.com>
2025-03-25 17:09:39 +0900

    ec2

--- terraform_stage/main.tf
+++ terraform_stage/main.tf
@@ -45,7 +45,12 @@ module "vpc" {
 }
 
 
-
+module "iam-service-role" {
+  source      = "../modules/iam/iam-service-role"
+  stage       = var.stage
+  servicename = var.servicename
+  tags        = var.tags
+}
 
 module "asw-ec2" {
   source              = "../modules/instance"
@@ -59,7 +64,7 @@ module "asw-ec2" {
   ebs_size                  = var.instance_ebs_size
   #user_data                 = var.instance_user_data
   kms_key_id                = var.ebs_kms_key_id
-  ec2-iam-role-profile-name = module.iam-service-role.ec2_iam_role_profile_name
+  ec2-iam-role-profile-name = module.iam-service-role.ec2-iam_role-profile-name
   ssh_allow_comm_list       = [var.subnet_service_az1, var.subnet_service_az2]
 
   associate_public_ip_address = var.associate_public_ip_address
